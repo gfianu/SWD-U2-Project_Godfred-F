41161139120ab873f8ecdf51e877e6a8d3af62c3
gfianu <delyouandme@gmail.com>
2026-03-01 16:58:58 -0500

    Springboot backend created

---
+++ OrgoMastery-Backend/orgomastery/src/test/java/com/example/orgomastery/OrgomasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orgomastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgomasteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
